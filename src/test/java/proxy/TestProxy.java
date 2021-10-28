62d96dd4bab61be68e3393de22bae5aff79048af
Pavel Chupikov <toppmote@gmail.com>
2021-10-29 03:12:33 +0700

    Add test for Proxy

---
+++ src/test/java/proxy/TestProxy.java
@@ -0,0 +1,35 @@
+package proxy;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import systemClasses.Message;
+import systemClasses.User;
+import systemClasses.algorithms.FirstAlgorithm;
+import systemClasses.filters.FilterObj;
+import systemClasses.messageContent.Text;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestProxy {
+
+    private static User user1, user2, user3;
+
+    @BeforeAll
+    static void initUsers() {
+        user1 = new User("User1", "01.01.2021");
+        user2 = new User("User2", "02.03.2021");
+        user3 = new User("User3", "03.03.2021");
+    }
+
+    @Test
+    void testProxy() {
+        System.out.println("-----------PROXY TEST-----------");
+        FilterObj mainFilter = new FilterObj(new FirstAlgorithm());
+        ProxyFilter proxyFilter = new ProxyFilter(mainFilter);
+        List<User> userList = Arrays.asList(user2, user3);
+        proxyFilter.getResult(user1, new Message(user1, userList, new Text("Some text")));
+        System.out.println("--------------------------------");
+    }
+
+}
