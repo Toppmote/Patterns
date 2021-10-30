b445324846a641f714282e00e31a8baffa9f89dc
Pavel Chupikov <toppmote@gmail.com>
2021-10-30 16:52:03 +0700

    Add Iterator test

---
+++ src/test/java/iterator/TestIterator.java
@@ -0,0 +1,29 @@
+package iterator;
+
+import org.junit.jupiter.api.Test;
+import systemClasses.SystemManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestIterator {
+
+    static int USER_COUNT = 5;
+
+    @Test
+    void testIterator() {
+        System.out.println("-----------ITERATOR TEST-----------");
+        SystemManager systemManager = new SystemManager();
+        for (int i = 0; i < USER_COUNT; i++)
+            systemManager.newUserRegistration("User" + (i + 1), i + "." + i + ".2021");
+        Iterator<?> iterator = systemManager.createIterator();
+        int iterUsersCount = 0;
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            iterUsersCount++;
+        }
+        assertEquals(USER_COUNT, iterUsersCount);
+        assertThrows(ArrayIndexOutOfBoundsException.class, iterator::next);
+        System.out.println("-----------------------------------");
+    }
+
+}
