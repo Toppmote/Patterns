ab794a86c169dade644679009999f72057da4450
Pavel Chupikov <toppmote@gmail.com>
2021-10-30 16:51:52 +0700

    Add Iterator pattern

---
+++ src/main/java/iterator/IterableCollection.java
@@ -0,0 +1,14 @@
+package iterator;
+
+/**
+ * Интерфейс получения итератора из коллекции-реализации интерфейса {@link java.util.List}
+ */
+public interface IterableCollection {
+
+    /**
+     * Метод получения итератора
+     * @return Итератор коллекции
+     */
+    Iterator<?> createIterator();
+
+}
