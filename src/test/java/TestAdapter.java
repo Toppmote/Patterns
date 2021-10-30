ef5c23916751a2eaec481dfed18d29c075fb99a4
Pavel Chupikov <toppmote@gmail.com>
2021-10-30 22:10:35 +0700

    Add test for Adapter

---
+++ src/test/java/TestAdapter.java
@@ -0,0 +1,16 @@
+import adapter.PdfAdapter;
+import adapter.PdfFile;
+import org.junit.jupiter.api.Test;
+
+public class TestAdapter {
+
+    @Test
+    void testAdapter() {
+        System.out.println("-----------ADAPTER TEST-----------");
+        PdfFile pdfFile = new PdfFile("New file", "This is a new file");
+        PdfAdapter adapter = new PdfAdapter(pdfFile);
+        System.out.println(pdfFile.getTitle() + " hash code: " + adapter.getContentHashCode());
+        System.out.println("----------------------------------");
+    }
+
+}
