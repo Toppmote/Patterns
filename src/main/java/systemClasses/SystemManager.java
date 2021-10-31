5bde80472f6375ed84341e5467a74023196417d4
Pavel Chupikov <toppmote@gmail.com>
2021-11-01 02:00:31 +0700

    Add new methods for Facade test

--- src/main/java/systemClasses/SystemManager.java
+++ src/main/java/systemClasses/SystemManager.java
@@ -3,12 +3,12 @@ package systemClasses;
 import iterator.IterableCollection;
 import iterator.Iterator;
 import iterator.ListIterator;
-import lombok.Getter;
-import lombok.Setter;
 import systemClasses.filters.Filter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Класс менеджера системы
@@ -18,14 +18,11 @@ public class SystemManager implements IterableCollection {
     /**
      * Список пользователей
      */
-    @Getter
     private final List<User> userList;
 
     /**
      * Фильтр, используемый системой
      */
-    @Setter
-    @Getter
     private Filter filter;
 
     public SystemManager() {
@@ -35,7 +32,8 @@ public class SystemManager implements IterableCollection {
 
     /**
      * Процедура регистрации нового пользователя
-     * @param FIO ФИО
+     *
+     * @param FIO     ФИО
      * @param regDate дата регистрации
      */
     public void newUserRegistration(String FIO, String regDate) {
@@ -50,5 +48,53 @@ public class SystemManager implements IterableCollection {
     @Override
     public Iterator<?> createIterator() {
         return new ListIterator<>(userList);
+    }
+
+    /**
+     * Метод поиска пользователей по имени
+     *
+     * @param FIO ФИО для поиска
+     * @return список найденных пользователей
+     */
+    public List<User> findUsersByFIO(String FIO) {
+        return userList.stream()
+                .filter(user -> user.FIO.equals(FIO))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Метод поиска пользователей по имени
+     *
+     * @param id ID для поиска
+     * @return найденный пользователь
+     */
+    public Optional<User> findUsersByID(int id) {
+        return userList.stream()
+                .filter(currUser -> currUser.getId() == id)
+                .findFirst();
+    }
+
+    /**
+     * Метод поиска результатов фильтрации по ID пользователя
+     *
+     * @param id ID для поиска
+     * @return найденный пользователь
+     */
+    public List<FilterResult> findFilterResultByUserID(int id) {
+        Optional<User> user = this.findUsersByID(id);
+        return user.map(value -> filter.findFilterResultByUser(value)).orElse(null);
+    }
+
+    /**
+     * Метод добавления нового пользователя в список пользователей
+     *
+     * @param user новый пользователь
+     */
+    public void addUser(User user) {
+        this.userList.add(user);
+    }
+
+    public void setFilter(Filter filter) {
+        this.filter = filter;
     }
 }
