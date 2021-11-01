f915bde7cd4f3b310bef6f8c23f5293877bc3650
Pavel Chupikov <toppmote@gmail.com>
2021-11-02 00:38:22 +0700

    Refactor test for actions

--- src/test/java/TestBridge.java
+++ src/test/java/TestBridge.java
@@ -1,19 +1,31 @@
+import bridge.Framework;
 import bridge.Hibernate;
 import bridge.Lombok;
 import org.junit.jupiter.api.Test;
-import systemClasses.SystemManager;
+import systemClasses.User;
+import systemClasses.userActions.AddNewFriendAction;
+import systemClasses.userActions.DoNewPostAction;
+import systemClasses.userActions.UserAction;
 
 public class TestBridge {
 
     @Test
     void testBridge() {
         System.out.println("-----------BRIDGE TEST-----------");
-        SystemManager manager = new SystemManager(new Hibernate());
-        manager.frameworkOperation();
-
-        manager.setFramework(new Lombok());
-        manager.frameworkOperation();
+        testFramework(new Hibernate());
+        testFramework(new Lombok());
         System.out.println("---------------------------------");
     }
+
+    void testFramework(Framework framework) {
+        System.out.println("\nAdd new friend action:");
+        User user = new User("user", "02.11.2021");
+        UserAction action = new AddNewFriendAction("02.11.2021", framework);
+        action.doAction(user);
+        System.out.println("\nDo new post action");
+        action = new DoNewPostAction("02.11.2021", framework);
+        action.doAction(user);
+    }
+
 
 }
