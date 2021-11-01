30a07065a2a7f391ae160ace06ca1688118a7570
Pavel Chupikov <toppmote@gmail.com>
2021-11-02 05:13:45 +0700

    Added Prototype pattern

--- src/main/java/systemClasses/Message.java
+++ src/main/java/systemClasses/Message.java
@@ -2,8 +2,10 @@ package systemClasses;
 
 import lombok.Getter;
 import lombok.Setter;
+import prototype.CloneableObj;
 import systemClasses.messageContent.MessageContent;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,8 +13,7 @@ import java.util.stream.Collectors;
 /**
  * Класс, описывающий сообщение
  */
-@Getter
-public class Message {
+public class Message implements CloneableObj {
 
     /**
      * Отправитель
@@ -27,7 +28,6 @@ public class Message {
     /**
      * Контент сообшения
      */
-    @Setter
     private List<MessageContent> messageContent;
 
     public Message(User sender, List<User> recipientList, MessageContent... messageContent) {
@@ -36,4 +36,30 @@ public class Message {
         this.messageContent = Arrays.stream(messageContent).collect(Collectors.toList());
     }
 
+    public Message(Message message) {
+        this.sender = message.getSender();
+        this.recipientList = new ArrayList<>(message.getRecipientList());
+        this.messageContent = new ArrayList<>(message.getMessageContent());
+    }
+
+    @Override
+    public CloneableObj clone() {
+        return new Message(this);
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public List<User> getRecipientList() {
+        return recipientList;
+    }
+
+    public List<MessageContent> getMessageContent() {
+        return messageContent;
+    }
+
+    public void setMessageContent(List<MessageContent> messageContent) {
+        this.messageContent = messageContent;
+    }
 }
