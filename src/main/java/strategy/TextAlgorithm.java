74aa191b388243227c232c8c38159c2cbaa03c78
Pavel Chupikov <toppmote@gmail.com>
2021-11-14 23:15:03 +0700

    Refactored name and method log

--- src/main/java/strategy/TextAlgorithm.java
+++ src/main/java/strategy/TextAlgorithm.java
@@ -11,13 +11,13 @@ import java.util.Random;
  *
  * @see Algorithm
  */
-public class TextAlgorithms implements Algorithm {
+public class TextAlgorithm implements Algorithm {
 
     @Override
     public FilterResult runAlgorithm(User user, Message message) {
         int max = 100;
         Random random = new Random();
-        System.out.println("Second algorithm finished work");
+        System.out.println("Text algorithm finished work");
         return new FilterResult(user, message, random.nextInt(max + 1));
     }
 }
